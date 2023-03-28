e316e2ed7186b89048dd79d9c18f50e3717a063d
dopeboy321 <105025755+dopeboy321@users.noreply.github.com>
2023-03-28 18:03:52 +0200

    added logic for dates

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ function AnkitClock() {
   const hour = clock.getHours();
   const min = clock.getMinutes();
   const sec = clock.getSeconds();
+  const day = clock.getDay();
+  const month = clock.getMonth();
+  const options = {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  }
+  const intlDate = Intl.DateTimeFormat("en-GB", options).format(clock);
+  const day__el = document.querySelector('.day');
+  const month__el = document.querySelector('.month');
   const hour__el = document.querySelector('.hours');
   const min__el = document.querySelector('.min');
   const sec__el = document.querySelector('.sec');
 
+
+  month__el.textContent = intlDate;
   hour__el.textContent = hour;
   min__el.textContent = `${min}`.padStart(2,0);
   sec__el.textContent = `${sec}`.padStart(2,0);
+  
 };
 setInterval(AnkitClock, 1000);
-
 
 
