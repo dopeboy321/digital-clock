abcc98246fbcb88291cbb46a0d0b81cfaa23efd3
dopeboy321 <105025755+dopeboy321@users.noreply.github.com>
2023-03-30 17:54:55 +0200

    logic for pm/am

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function AnkitClock() {
   const month = clock.getMonth();
   const options = {
     day: "numeric",
-    month: "long",
+    month: "short",
     year: "numeric"
   }
   const intlDate = Intl.DateTimeFormat("en-GB", options).format(clock);
@@ -16,13 +16,16 @@ function AnkitClock() {
   const hour__el = document.querySelector('.hours');
   const min__el = document.querySelector('.min');
   const sec__el = document.querySelector('.sec');
-
+  const am_pm__el = document.querySelector('.pm_am')
 
   month__el.textContent = intlDate;
   hour__el.textContent = hour;
   min__el.textContent = `${min}`.padStart(2,0);
   sec__el.textContent = `${sec}`.padStart(2,0);
-  
+  am_pm__el.textContent =  hour < 12 ? "AM" : "PM";
+  // const age = 18;
+  // console.log(age>= 18 ? "You can have driving lisence" : "NO you can't drive");
+
 };
 setInterval(AnkitClock, 1000);
 
